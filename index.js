6803ee890e4a42667afd44a7f9d083ec1b7706d4
MORRIS <morris24kariuki@gmail.com>
2022-11-24 02:24:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+main.remove();
+
+
+const newHeader = document.createElement("h1");
+
+
+newHeader.id = 'victory';
+
+document.getElementById("victory");
+newHeader.innerHTML = "Morris is the champion";
